3243fc6504212ca4a4bb465434bddb331064e413
Juan Carlos Avila <juanvs23@gmail.com>
2022-07-09 20:14:31 -0400

    change port

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const routes = require("./router");
 const cors = require("cors");
 const server = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 server.use(express.json());
 server.use("/", routes());
-server.listen(port, () => {
+server.listen(process.env.PORT || 5500, () => {
   console.log(`Example app listening on port ${port}`);
 });
